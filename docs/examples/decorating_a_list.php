424fc02317b8d9906a9c06fa2e53522676b4a825
Michael Wallner <mike@php.net>
2004-05-05 10:23:13 +0000

    * added another DecoratedList example
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk@158005 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ docs/examples/decorating_a_list.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+* Decorating a List
+* =================
+*
+* I18Nv2 provides decorated classes for country and language lists.
+* There's just another way to use them like in using_I18Nv2_DecoratedList.php
+* 
+* $Id$
+*/
+
+require_once 'I18Nv2/Country.php';
+
+$c = &new I18Nv2_Country('fr', 'iso-8859-1');
+$s = &$c->toDecoratedList('HtmlSelect');
+
+// set some attributes
+$s->attributes['select']['name'] = 'CountrySelect';
+$s->attributes['select']['onchange'] = 'this.form.submit()';
+
+// set a selected entry
+$s->selected['DE'] = true;
+
+// print a HTML safe select box
+echo $s->getAllCodes();
+?>
