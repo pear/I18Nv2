1175ace77963295f3740ab9b98c3f47f25d4aff6
Michael Wallner <mike@php.net>
2004-05-08 12:28:10 +0000

    * phpdoc * added ZE2 constructors * ICUParser::toks => ICUParser::buffer * RootNode::addChild() now returns a ref to the added child
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk@158196 c90b9560-bf6c-de11-be94-00142212c4b1

--- OpenI18N/ICUParser.php
+++ OpenI18N/ICUParser.php
@@ -1,18 +1,69 @@
 <?php
 
+/**
+* I18Nv2_OpenI18N_ICUParser
+* 
+* @author   Michael Wallner <mike@php.net>
+* @package  I18Nv2
+* @version  $Revision$
+* @access   public
+*/
 class I18Nv2_OpenI18N_ICUParser
 {
-    
+    /**
+    * Reference to the current node in the tree
+    * 
+    * @var      object
+    * @access   protected
+    */
     var $node;
-    var $toks = '';
+    
+    /**
+    * ICU tree
+    * 
+    * @var      object
+    * @access   protected
+    */
     var $tree;
     
+    /**
+    * String buffer
+    * 
+    * @var      string
+    * @access   protected
+    */
+    var $buffer = '';
+    
+    /**
+    * Constructor
+    * 
+    * Initializes the tree with an ICURootNode.
+    * 
+    * @access   public
+    * @return   object
+    */
     function I18Nv2_OpenI18N_ICUParser()
+    {
+        I18Nv2_OpenI18N_ICUParser::__construct();
+    }
+    
+    /**
+    * ZE2 Constructor
+    * @ignore
+    */
+    function __construct()
     {
         $this->tree = &new I18Nv2_OpenI18N_ICURootNode;
         $this->node = &$this->tree;
     }
     
+    /**
+    * Parse ICU string
+    * 
+    * @access   public
+    * @return   int     string length
+    * @param    string  $str
+    */
     function parse($str)
     {
         $count = 0;
@@ -24,68 +75,148 @@ class I18Nv2_OpenI18N_ICUParser
             switch ($token)
             {
                 case '{':
-                     $c = $this->node->addChild(
-                        new I18Nv2_OpenI18N_ICUTreeNode($this->toks)
+                     $this->node = &$this->node->addChild(
+                        new I18Nv2_OpenI18N_ICUTreeNode($this->buffer)
                      );
-                     $this->node = &$this->node->childs[$c];
                 break;
                 
                 case '}':
-                    $this->node->setData($this->toks);
+                    $this->node->setData($this->buffer);
                     $this->node = &$this->node->parent;
                 break;
                 
                 default:
-                     $this->toks .= ($token);
+                     $this->buffer .= ($token);
                 break;
             }
         }
+        return $length;
+    }
+    
+    /**
+    * Get the tree
+    * 
+    * @access   public
+    * @return   array
+    * @param    bool    $asArray
+    */
+    function getTree($asArray = true)
+    {
+        return $asArray ? $this->tree->toArray() :  $this->tree->getChildren();
     }
 }
 
+/**
+* I18Nv2_OpenI18N_ICURootNode
+* 
+* @author   Michael Wallner <mike@php.net>
+* @package  I18Nv2
+* @version  $Revision$
+* @access   protected
+*/
 class I18Nv2_OpenI18N_ICURootNode
 {
+    /**#@+
+    * @access   protected
+    */
     var $childs = array();
     var $data = '';
     var $name = '__ROOT__';
+    /**#@-**/
     
+    /**
+    * ZE2 Constructor
+    * @ignore
+    */
+    function __construct() {}
+    
+    /**
+    * Set payload of the treenode
+    * 
+    * Supplied data will be truncated.
+    * 
+    * @access   public
+    * @return   void
+    * @param    string  $data
+    */
     function setData(&$data)
     {
         $this->data = trim($data);
         $data = '';
     }
     
-    function addChild(&$child)
+    /**
+    * Add a ICUTreeNode child
+    * 
+    * Returns the child supplied as parameter.
+    * 
+    * @access   public
+    * @return   object
+    * @param    object
+    */
+    function &addChild(&$child)
     {
         if (!is_a($child, 'I18Nv2_OpenI18N_ICUTreeNode')) {
-            return -1;
+            return null;
         }
-        $count = count($this->childs);
-        $this->childs[$count] = &$child;
         $child->setParent($this);
-        return $count;
+        $this->childs[] = &$child;
+        return $child;
     }
     
+    /**
+    * Get parent tree node
+    * 
+    * The RootNode will return a reference to itself.
+    * 
+    * @access   public
+    * @return   object
+    */
     function &getParent()
     {
         return $this;
     }
     
+    /**
+    * Check wheter this node is a RootNode
+    * 
+    * @access   public
+    * @return   bool
+    */
     function isRootNode()
     {
         return (strToLower(get_class($this)) === 'i18nv2_openi18n_icurootnode');
     }
     
+    /**
+    * Get children
+    * 
+    * @access   public
+    * @return   array
+    */
     function getChildren()
     {
         return $this->childs;
     }
     
+    /**
+    * Get trees data as array
+    * 
+    * @access   public
+    * @return   array
+    */
     function toArray()
     {
         return $this->_getChilds($this->childs);
     }
     
+    /**
+    * Tree ==> Array
+    * 
+    * @access   private
+    * @return   array
+    * @param    array   $childs
+    */
     function _getChilds($childs)
     {
         $result = array();
@@ -100,16 +231,53 @@ class I18Nv2_OpenI18N_ICURootNode
     }
 }
 
+/**
+* I18Nv2_OpenI18N_ICUTreeNode
+* 
+* @author   Michael Wallner <mike@php.net>
+* @package  I18Nv2
+* @version  $Revision$
+* @access   protected
+*/
 class I18Nv2_OpenI18N_ICUTreeNode extends I18Nv2_OpenI18N_ICURootNode
 {
+    /**
+    * Parent Node
+    * @var  object
+    */
     var $parent;
     
+    /**
+    * Constructor
+    * 
+    * Supplied variable gets truncated.
+    * 
+    * @access   public
+    * @return   object
+    * @param    string  $name
+    */
     function I18Nv2_OpenI18N_ICUTreeNode(&$name)
+    {
+        I18Nv2_OpenI18N_ICUTreeNode::__construct($name);
+    }
+    
+    /**
+    * ZE2 Constructor
+    * @ignore
+    */
+    function __construct(&$name)
     {
         $this->name = trim($name);
         $name = '';
     }
     
+    /**
+    * Set parent node
+    * 
+    * @access   public
+    * @return   bool
+    * @param    object
+    */
     function setParent(&$parent)
     {
         if ($isRootNode = is_a($parent, 'I18Nv2_OpenI18N_ICURootNode')) {
@@ -118,16 +286,21 @@ class I18Nv2_OpenI18N_ICUTreeNode extends I18Nv2_OpenI18N_ICURootNode
         return $isRootNode;
     }
     
+    /**
+    * Get parent node
+    * 
+    * @access   public
+    * @return   object
+    */
     function &getParent()
     {
         return $this->parent;
     }
 }
 
-/*
 $t = &new I18Nv2_OpenI18N_ICUParser();
 $t->parse(' ICU { de{ "DE", "Deutsch" } en { "EN", "Englisch" } es { territory {"ES", "Spanien"} currency {"ESP", "Spanischer Peso"} }}');
 #var_dump($t->tree);
 print_r($t->tree->toArray());
-*/
+
 ?>
