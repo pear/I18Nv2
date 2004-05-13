dd41f4c372f6532b07f8c1882a676652193132c1
Michael Wallner <mike@php.net>
2004-05-13 19:27:42 +0000

    * files moved from I18Nv2/Locale to I18Nv2/Locale/libc
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk@158524 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Locale/libc/EUR.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* $Id$
+*/
+
+$this->formats['currency'][I18Nv2_CURRENCY_LOCAL][0] = '€';
+$this->formats['currency'][I18Nv2_CURRENCY_LOCAL][1] = 2;
+
+$this->formats['currency'][I18Nv2_CURRENCY_INTERNATIONAL][0] = 'EUR';
+$this->formats['currency'][I18Nv2_CURRENCY_INTERNATIONAL][1] = 2;
+$this->formats['currency'][I18Nv2_CURRENCY_INTERNATIONAL][4] = '-';
+$this->formats['currency'][I18Nv2_CURRENCY_INTERNATIONAL][5] = '';
+$this->formats['currency'][I18Nv2_CURRENCY_INTERNATIONAL][6] = true;
+$this->formats['currency'][I18Nv2_CURRENCY_INTERNATIONAL][7] = true;
+$this->formats['currency'][I18Nv2_CURRENCY_INTERNATIONAL][8] = true;
+$this->formats['currency'][I18Nv2_CURRENCY_INTERNATIONAL][9] = false;
+
+?>
