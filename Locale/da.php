e7a75bfbda394b7f629687b57b81abc497f1b8ed
Michael Wallner <mike@php.net>
2004-06-01 17:55:04 +0000

    * reimport locale extensions
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk@160305 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Locale/da.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* $Id$
+*/
+
+$this->_dateFormats = array(
+    I18Nv2_DATETIME_SHORT     =>  '%e/%m-%y',
+    I18Nv2_DATETIME_DEFAULT   =>  '%d-%b-%Y',
+    I18Nv2_DATETIME_MEDIUM    =>  '%d-%b-%Y',
+    I18Nv2_DATETIME_LONG      =>  '%e. %B %Y',
+    I18Nv2_DATETIME_FULL      =>  '%A, d. %e. %B Y'
+);
+$this->_timeFormats = array(
+    I18Nv2_DATETIME_SHORT     =>  'H:i',
+    I18Nv2_DATETIME_DEFAULT   =>  '%H:%M:%S',
+    I18Nv2_DATETIME_MEDIUM    =>  '%H:%M:%S',
+    I18Nv2_DATETIME_LONG      =>  '%H:%M:%S %Z',
+    I18Nv2_DATETIME_FULL      =>  'kl. %H:%M'
+);      
+?>
