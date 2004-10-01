5b2b4bab6b5798b6475c69cd347039f96f58868d
Michael Wallner <mike@php.net>
2004-10-01 15:08:51 +0000

    * squeeze in filter example
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk@169738 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/decorating_a_list.php
+++ docs/examples/decorating_a_list.php
@@ -13,7 +13,13 @@
 require_once 'I18Nv2/Country.php';
 
 $c = &new I18Nv2_Country('it', 'iso-8859-1');
-$s = &$c->toDecoratedList('HtmlSelect');
+
+// Filter
+$f = &$c->toDecoratedList('Filter');
+$f->elements = array('it', 'fr', 'de', 'ch', 'at', 'es', 'lu', 'nl', 'be', 'gb');
+
+// HtmlSelect
+$s = &$f->toDecoratedList('HtmlSelect');
 
 // set some attributes
 $s->attributes['select']['name'] = 'CountrySelect';
@@ -22,6 +28,6 @@ $s->attributes['select']['onchange'] = 'this.form.submit()';
 // set a selected entry
 $s->selected['DE'] = true;
 
-// print a HTML safe select box
+// print a HTML select box
 echo $s->getAllCodes();
 ?>
