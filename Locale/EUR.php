49da978dcd971429b31c31f477010699440ce02c
Michael Wallner <mike@php.net>
2004-10-08 08:59:10 +0000

    * EUR character has been CP1252 encoded
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk@170056 c90b9560-bf6c-de11-be94-00142212c4b1

--- Locale/EUR.php
+++ Locale/EUR.php
@@ -1,9 +1,11 @@
 <?php
 /**
 * $Id$
+* 
+* @charset ISO-8859-15
 */
 
-$this->_currencyFormats[I18Nv2_CURRENCY_LOCAL][0] = '€';
+$this->_currencyFormats[I18Nv2_CURRENCY_LOCAL][0] = chr(0xA4);
 $this->_currencyFormats[I18Nv2_CURRENCY_LOCAL][1] = 2;
 
 $this->_currencyFormats[I18Nv2_CURRENCY_INTERNATIONAL][0] = 'EUR';
