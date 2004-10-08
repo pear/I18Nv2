135e415dbf0aa0df4ec266144cc02a26a29c9808
Michael Wallner <mike@php.net>
2004-10-08 08:33:14 +0000

    * fix sep_by_space for posititve values
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk@170054 c90b9560-bf6c-de11-be94-00142212c4b1

--- Locale/EUR.php
+++ Locale/EUR.php
@@ -13,6 +13,6 @@ $this->_currencyFormats[I18Nv2_CURRENCY_INTERNATIONAL][5] = '';
 $this->_currencyFormats[I18Nv2_CURRENCY_INTERNATIONAL][6] = true;
 $this->_currencyFormats[I18Nv2_CURRENCY_INTERNATIONAL][7] = true;
 $this->_currencyFormats[I18Nv2_CURRENCY_INTERNATIONAL][8] = true;
-$this->_currencyFormats[I18Nv2_CURRENCY_INTERNATIONAL][9] = false;
+$this->_currencyFormats[I18Nv2_CURRENCY_INTERNATIONAL][9] = true;
 
 ?>
