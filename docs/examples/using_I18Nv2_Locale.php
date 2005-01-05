489c7456237819e590bd34b27c718db3da768014
Michael Wallner <mike@php.net>
2005-01-05 09:26:18 +0000

    * display another locale, too
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk@176494 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/using_I18Nv2_Locale.php
+++ docs/examples/using_I18Nv2_Locale.php
@@ -14,6 +14,7 @@ require_once 'I18Nv2.php';
 
 $locale = &I18Nv2::createLocale('de_AT');
 
+echo "de_AT\n=====\n";
 echo "Format a currency value of 2000: ",
     $locale->formatCurrency(2000, I18Nv2_CURRENCY_INTERNATIONAL), "\n";
 
@@ -22,4 +23,18 @@ echo "Format todays date:              ",
 
 echo "Format current time:             ",
     $locale->formatTime(null, I18Nv2_DATETIME_SHORT), "\n";
+
+
+$locale->setLocale('en_GB');
+
+echo "\nen_GB\n=====\n";
+echo "Format a currency value of 2000: ",
+    $locale->formatCurrency(2000, I18Nv2_CURRENCY_INTERNATIONAL), "\n";
+
+echo "Format todays date:              ",
+    $locale->formatDate(null, I18Nv2_DATETIME_FULL), "\n";
+
+echo "Format current time:             ",
+    $locale->formatTime(null, I18Nv2_DATETIME_SHORT), "\n";
+
 ?>
