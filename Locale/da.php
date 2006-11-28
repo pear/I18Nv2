3f3b53c3ab5c590ae01633a5ec37e77e3057a28e
Lorenzo Alberton <quipo@php.net>
2006-11-28 17:01:33 +0000

    missing %
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk@224000 c90b9560-bf6c-de11-be94-00142212c4b1

--- Locale/da.php
+++ Locale/da.php
@@ -8,10 +8,10 @@ $this->dateFormats = array(
     I18Nv2_DATETIME_DEFAULT   =>  '%d-%b-%Y',
     I18Nv2_DATETIME_MEDIUM    =>  '%d-%b-%Y',
     I18Nv2_DATETIME_LONG      =>  '%e. %B %Y',
-    I18Nv2_DATETIME_FULL      =>  '%A, d. %e. %B Y'
+    I18Nv2_DATETIME_FULL      =>  '%A, d. %d. %B %Y'
 );
 $this->timeFormats = array(
-    I18Nv2_DATETIME_SHORT     =>  'H:i',
+    I18Nv2_DATETIME_SHORT     =>  '%H:%M',
     I18Nv2_DATETIME_DEFAULT   =>  '%H:%M:%S',
     I18Nv2_DATETIME_MEDIUM    =>  '%H:%M:%S',
     I18Nv2_DATETIME_LONG      =>  '%H:%M:%S %Z',
