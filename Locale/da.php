3faf062087547c042d4afb1cb2936832e7e035d4
Daniel O'Connor <clockwerx@php.net>
2010-02-15 06:34:36 +0000

    Updated svn layout
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk/I18Nv2@295088 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Locale/da.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* $Id$
+*/
+
+$this->dateFormats = array(
+    I18Nv2_DATETIME_SHORT     =>  '%e/%m-%y',
+    I18Nv2_DATETIME_DEFAULT   =>  '%d-%b-%Y',
+    I18Nv2_DATETIME_MEDIUM    =>  '%d-%b-%Y',
+    I18Nv2_DATETIME_LONG      =>  '%e. %B %Y',
+    I18Nv2_DATETIME_FULL      =>  '%A, den %d. %B %Y'
+);
+$this->timeFormats = array(
+    I18Nv2_DATETIME_SHORT     =>  '%H:%M',
+    I18Nv2_DATETIME_DEFAULT   =>  '%H:%M:%S',
+    I18Nv2_DATETIME_MEDIUM    =>  '%H:%M:%S',
+    I18Nv2_DATETIME_LONG      =>  '%H:%M:%S %Z',
+    I18Nv2_DATETIME_FULL      =>  'kl. %H:%M'
+);      
+?>
