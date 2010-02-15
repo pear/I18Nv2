4f15c7fb9dfca4cb930bad1fb960693c501b1b0e
Daniel O'Connor <clockwerx@php.net>
2010-02-15 06:34:18 +0000

    Updated svn layout
    git-svn-id: http://svn.php.net/repository/pear/packages/I18Nv2/trunk/I18Nv2@295087 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Region/US/fr.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * $Id$
+ */
+$this->codes = array(
+    'AL' => 'Alabama',
+    'AK' => 'Alaska',
+    'AZ' => 'Arizona',
+    'AR' => 'Arkansas',
+
+    'CA' => 'Californie',
+    'CO' => 'Colorado',
+    'CT' => 'Connecticut',
+
+    'DE' => 'Delaware',
+    'DC' => 'District of Columbia',
+
+    'FL' => 'Floride',
+
+    'GA' => 'Géorgie',
+
+    'HI' => 'Hawaï',
+
+    'ID' => 'Idaho',
+    'IL' => 'Illinois',
+    'IN' => 'Indiana',
+    'IA' => 'Iowa',
+
+    'KS' => 'Kansas',
+    'KY' => 'Kentucky',
+
+    'LA' => 'Louisiane',
+
+    'ME' => 'Maine',
+    'MD' => 'Maryland',
+    'MA' => 'Massachusetts',
+    'MI' => 'Michigan',
+    'MN' => 'Minnesota',
+    'MS' => 'Mississippi',
+    'MO' => 'Missouri',
+    'MT' => 'Montana',
+
+    'NE' => 'Nebraska',
+    'NV' => 'Nevada',
+    'NH' => 'New Hampshire',
+    'NJ' => 'New Jersey',
+    'NM' => 'Nouveau-Méxique',
+    'NY' => 'New York',
+    'NC' => 'Caroline du Nord',
+    'ND' => 'Dakota du Nord',
+
+    'OH' => 'Ohio',
+    'OK' => 'Oklahoma',
+    'OR' => 'Oregon',
+
+    'PA' => 'Pennsylvanie',
+    'RI' => 'Rhode Island',
+
+    'SC' => 'Caroline du Sud',
+    'SD' => 'Dakota du Sud',
+
+    'TN' => 'Tennessee',
+    'TX' => 'Texas',
+
+    'UT' => 'Utah',
+
+    'VT' => 'Vermont',
+    'VA' => 'Virginie',
+
+    'WA' => 'Washington',
+    'WV' => 'Virginie-Occidentale',
+    'WI' => 'Wisconsin',
+    'WY' => 'Wyoming'
+);
+
+?>
